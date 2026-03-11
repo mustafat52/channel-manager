006237200624f54d56274e212163ff547716eaf7
MustafaT52 <mustafataj13@gmail.com>
2026-03-11 15:17:14 +0530

    do

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Read DATABASE_URL from environment
 DATABASE_URL = os.getenv("DATABASE_URL")
