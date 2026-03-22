f5bedbe209765d6248b5784550805e12e09d16db
MustafaT52 <mustafataj13@gmail.com>
2026-03-22 14:37:34 +0530

    db shift

--- app/db/database.py
+++ app/db/database.py
@@ -11,7 +11,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in environment variables.")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
+    
 # Create SQLAlchemy engine
 engine = create_engine(
     DATABASE_URL,
